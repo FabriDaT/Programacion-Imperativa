2545b8dfd213d08254d9d3673b2d42236217fa5b
FabricioDa <fabriciodanielturrina@gmail.com>
2022-05-25 17:29:25 -0300

    commit

---
+++ clase4.js
@@ -0,0 +1,14 @@
+// situacion = llamar a un compa para saber si esta listo para que nos lleve al trabajo
+
+let creditoCelular = true/false
+let companiero1 = 'jose'
+const celularCompaniero1 =  2645484944
+const companiero2 = 'diego'
+const celularCompaniero2 =  2646474512
+const companiera3 = 'luz'
+const celularCompaniero3 =  2646457789
+let trafico = true/false
+let llueve = true/false
+let piquete = true/false
+let TiempoDeLlegada = 20
+
