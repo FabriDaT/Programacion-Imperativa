df85e438d3dcb824433f494f6caa7daf0b3be0ab
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2022-06-14 13:01:28 -0300

    hola

--- clase10.js
+++ clase10.js
@@ -1,15 +1,47 @@
-let pelis = ["star wars", "totoro", "rocky", "pulp fiction", "la vida es bella"]
-console.log(pelis[4])
 
 let Uppercase = string => string.toUpperCase()
-console.log(Uppercase('paso los dias JIJI JAJA !!'))
+console.log(Uppercase('paso los dias jiji jaja !!'))
  
 let animadas = ["toy story", "finding Nemo", "kung-fu panda", "wally", "fortnite"]
-
-let unirArrays = (array1, array2) => {
-    let nuevoArray = []
-   
-    nuevoArray= array1.push( array2.toUpperCase())
-    return nuevoArray
+let pelis = ["star wars", "totoro", "rocky", "pulp fiction", "la vida es bella"]
+const pasarMayusculas = arr =>{
+    let arrMayus = arr.toString().toUpperCase()
+    return arrMayus
 }
-console.log(unirArrays(pelis, animadas)) 
+//console.log(pasarMayusculas(pelis)) 
+//console.log(pasarMayusculas(animadas)) 
+
+const unirArr = (arr1, arr2) => {
+    arr1.push(arr2)
+    const mayusculas = arr1.toString().toUpperCase()
+    console.log(mayusculas)
+}
+const sacarUltimo = array => {
+    let elUltimo = array.pop()
+}
+sacarUltimo(animadas)
+unirArr(pelis, animadas)
+
+const asiaScores = [1, 1, 7, 7, 5, 4, 4, 7, 1]
+const euroScores = [8, 1, 6, 9, 10, 6, 7, 9, 1]
+
+const compararResultados = (asiaScores, euroScores) =>{
+    for(let i = 0 ; i < 9; i++){
+        
+        if(asiaScores[i] != euroScores[i]){
+            console.log(`La calificacion numero ${i+1} es diferente`)
+        } else if(asiaScores[i] === euroScores[i]) {
+            console.log(`La calificacion numero ${i+1} esta empatada`)
+        } 
+    }
+}
+compararResultados(asiaScores,euroScores)
+
+
+let arregloAinvertir = [1,2,3,4,5,6,7,8,9,10]
+
+for( let i = arregloAinvertir.length ; i >= 0 ; i-- ){
+    console.log(arregloAinvertir[i])
+}
+let invertido = arregloAinvertir.reverse()
+console.log(invertido)
